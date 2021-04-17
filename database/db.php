3a18ead031238d15625fd2252103e5b997e2f7fc
lsp12 <jonathankenny852@gmail.com>
2021-04-16 21:16:22 -0500

    16/03/2021

--- database/db.php
+++ database/db.php
@@ -5,17 +5,17 @@
         $pass='29UlP2tmKT';
         $db='sql10365856';  */
 
-        $host='localhost';
+        /* $host='localhost';
         $user='root';
         $pass='';
         $db='emiliatur';
-        $puerto=null;
+        $puerto=null; */
 
-        /* $host='mysql-11088-0.cloudclusters.net';
+        $host='mysql-11088-0.cloudclusters.net';
         $user='vera';
         $pass='1207345768';
         $db='turismo_vera';
-        $puerto=11136; */
+        $puerto=11136;
         
         $mysqli = new mysqli($host,$user,$pass,$db,$puerto);
         if($mysqli->connect_errno){
